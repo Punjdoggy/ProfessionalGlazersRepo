d47ee0f1fbee0abab532584afa98c383dee920d8
Punjdoggy <112536823+Punjdoggy@users.noreply.github.com>
2024-05-07 21:11:05 +1000

    Spline Tinkering
    Spline code but still retains original linear code for MeepMeep auto

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,7 +1,11 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueDark;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedDark;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedLight;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
@@ -13,10 +17,49 @@ import javax.imageio.ImageIO;
 
 public class MeepMeepTesting {
     public static void main(String[] args) {
-        MeepMeep meepMeep = new MeepMeep(800);
+        MeepMeep meepMeep = new MeepMeep(900);
 
-        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+        RoadRunnerBotEntity RedBot1 = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                // Set bot colour: Blue Dark, Blue Light, Red Dark, Red Light
+                .setColorScheme(new ColorSchemeRedDark())
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(-40.81, -62.78, Math.toRadians(90.00)))
+                                .splineToLinearHeading(new Pose2d(19, -42.5,Math.toRadians(-90)), Math.toRadians(-180))
+                                .forward(10)
+                                .waitSeconds(5)
+                                .splineToSplineHeading(new Pose2d(0, -42.6, Math.toRadians(90)), Math.toRadians(0)) //ask lachie why it does a turn then does a rotation to forward
+                                .turn(Math.toRadians(90))
+                                .forward(20)
+                                .splineToSplineHeading(new Pose2d(-41.0,42.9, Math.toRadians(180)), Math.toRadians(90))
+                                .forward(17)
+                                .build()
+
+//                        original code
+//                                .turn(Math.toRadians(0))
+//                                .forward(20)
+//                                .turn(Math.toRadians(-90))
+//                                .forward(60)
+//                                .turn(Math.toRadians(-90))
+//                                .forward(10)
+//                                .waitSeconds(5)
+//                                .turn(Math.toRadians(-180))
+//                                .forward(10)
+//                                .turn(Math.toRadians(90))
+//                                .forward(25)
+//                                .turn(Math.toRadians(-90))
+//                                .forward(80)
+//                                .turn(Math.toRadians(90))
+//                                .forward(15)
+//                                .build()
+
+                );
+
+        RoadRunnerBotEntity RedBot2 = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                // Set bot colour: Blue Dark, Blue Light, Red Dark, Red Light
+                .setColorScheme(new ColorSchemeRedLight())
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive ->
                         drive.trajectorySequenceBuilder(new Pose2d(-40.81, -62.78, Math.toRadians(90.00)))
@@ -29,8 +72,34 @@ public class MeepMeepTesting {
                                 .waitSeconds(5)
                                 .turn(Math.toRadians(-180))
                                 .forward(10)
+                                .turn(Math.toRadians(-90))
+                                .forward(25)
+                                .turn(Math.toRadians(90))
+                                .forward(80)
+                                .turn(Math.toRadians(-90))
+                                .forward(15)
+
+                                .build()
+                );
+
+        RoadRunnerBotEntity BlueBot1 = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                // Set bot colour: Blue Dark, Blue Light, Red Dark, Red Light
+                .setColorScheme(new ColorSchemeBlueDark())
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(-40.81, 62.78, Math.toRadians(-90)))
+                                .turn(Math.toRadians(0))
+                                .forward(20)
                                 .turn(Math.toRadians(90))
                                 .forward(60)
+                                .turn(Math.toRadians(90))
+                                .forward(10)
+                                .waitSeconds(5)
+                                .turn(Math.toRadians(-180))
+                                .forward(10)
+                                .turn(Math.toRadians(90))
+                                .forward(25)
                                 .turn(Math.toRadians(-90))
                                 .forward(80)
                                 .turn(Math.toRadians(90))
@@ -39,14 +108,43 @@ public class MeepMeepTesting {
                                 .build()
                 );
 
+        RoadRunnerBotEntity BlueBot2 = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                // Set bot colour: Blue Dark, Blue Light, Red Dark, Red Light
+                .setColorScheme(new ColorSchemeBlueDark())
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(-40.81, 62.78, Math.toRadians(-90)))
+                                .turn(Math.toRadians(0))
+                                .forward(20)
+                                .turn(Math.toRadians(90))
+                                .forward(60)
+                                .turn(Math.toRadians(90))
+                                .forward(10)
+                                .waitSeconds(5)
+                                .turn(Math.toRadians(-180))
+                                .forward(10)
+                                .turn(Math.toRadians(-90))
+                                .forward(60)
+                                .turn(Math.toRadians(90))
+                                .forward(80)
+                                .turn(Math.toRadians(-90))
+                                .forward(15)
+
+                                .build()
+                );
+
         Image img = null;
-        try { img = ImageIO.read(new File("C:\\Users\\shubh133365\\OneDrive - Brisbane Grammar School\\Robotics\\Code\\Off-Season\\PostalPanic_Stage.png")); }
+        try { img = ImageIO.read(new File("MeepMeepTesting/src/resources/PostalPanic_Stage.png")); }
         catch (IOException e) {}
 
 
         meepMeep.setBackground(img)
                 .setBackgroundAlpha(0.95f)
-                .addEntity(myBot)
+                .addEntity(RedBot1)
+                .addEntity(RedBot2)
+                .addEntity(BlueBot1)
+                .addEntity(BlueBot2)
                 .start();
     }
 }
