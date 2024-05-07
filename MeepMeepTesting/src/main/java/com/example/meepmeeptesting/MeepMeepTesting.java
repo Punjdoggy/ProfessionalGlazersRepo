5f5b94b179cfe2057a8f4cdbfd46c3571c4d8356
Punjdoggy <112536823+Punjdoggy@users.noreply.github.com>
2024-05-07 17:03:36 +1000

    MeepMeep Adjusting

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -5,6 +5,12 @@ import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 public class MeepMeepTesting {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
@@ -13,21 +19,22 @@ public class MeepMeepTesting {
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
+                        drive.trajectorySequenceBuilder(new Pose2d(-40.81, -62.78, Math.toRadians(90.00)))
+                                .turn(Math.toRadians(0))
+                                .forward(110)
                                 .turn(Math.toRadians(-90))
-                                .forward(30)
+                                .forward(60)
                                 .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(-90))
-                                .forward(30)
-                                .turn(Math.toRadians(-90))
-                                .forward(30)
-                                .turn(Math.toRadians(-90))
+                                .forward(10)
                                 .build()
                 );
 
-        meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
-                .setDarkMode(true)
+        Image img = null;
+        try { img = ImageIO.read(new File("C:\\Users\\shubh133365\\OneDrive - Brisbane Grammar School\\Robotics\\Code\\Off-Season\\PostalPanic_Stage.png")); }
+        catch (IOException e) {}
+
+
+        meepMeep.setBackground(img)
                 .setBackgroundAlpha(0.95f)
                 .addEntity(myBot)
                 .start();
